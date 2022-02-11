71dbbc60d63cd8e450572fdbf7c06f986aa8681c
Yoshi K <kai.outsider.777+git@gmail.com>
2022-02-11 21:37:48 +0900

    flaskr 側の healthcheck を追加

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
+import { useState } from "react";
+import axios from "axios";
 import logo from 'logo.svg';
 import 'App.css';
 
 function App() {
+  const [readDatetime, setReadDatetime] = useState("-");
+  const [writeDatetime, setWriteDatetime] = useState("-");
+
+  const handleHealthCheck = async () => {
+    try {
+      const { data } = await axios.get('https://young-hollows-79466.herokuapp.com/api/v1/healthcheck');
+      setReadDatetime(data.read_datetime)
+      setWriteDatetime(data.write_datetime)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button type="button" className="btn btn-primary" onClick={handleHealthCheck}>health check!</button>
+
+          <table className="table table-light table-sm mt-3">
+            <thead>
+              <tr>
+                <th scope="col">Read Datetime</th>
+                <th scope="col">Write Datetime</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{readDatetime}</td>
+                <td>{writeDatetime}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className='mt-5'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
       </header>
     </div>
   );
