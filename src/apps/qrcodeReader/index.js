c0bc88d3eba4899ae14156a31d9db9ca398e4977
Yoshi K <kai.outsider.777+git@gmail.com>
2022-03-03 16:43:54 +0900

    requestAnimationFrame を使ったカウントアップを実装
    ref. https://css-tricks.com/using-requestanimationframe-with-react-hooks/
    ref. https://bom-shibuya.hatenablog.com/entry/2020/10/27/182226

--- src/apps/qrcodeReader/index.js
+++ src/apps/qrcodeReader/index.js
@@ -1,71 +1,116 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const QrcodeReader = () => {
-  const videoRef = useRef(null);
-  const photoRef = useRef(null);
+  const [count, setCount] = useState(0);
+  const previousTimeRef = useRef();
 
-  const [hasPhoto, setHasPhoto] = useState(false);
-
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width: 1920, height: 1080 } })
-      .then((stream) => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  const useAnimationFrame = (callback) => {
+    const requestRef = useRef();
+    const animate = useCallback(
+      (time) => {
+        callback(time);
+        requestRef.current = requestAnimationFrame(animate);
+      },
+      [callback]
+    );
+    useEffect(() => {
+      requestRef.current = requestAnimationFrame(animate);
+      return () => {
+        if (requestRef.current) {
+          return cancelAnimationFrame(requestRef.current);
+        }
+      };
+    }, [animate]);
   };
 
-  const takePhoto = () => {
-    const width = 414;
-    const height = width / (16 / 9);
+  useAnimationFrame((time) => {
+    if (previousTimeRef.current !== undefined) {
+      const deltaTime = time - previousTimeRef.current;
+      setCount((prevCount) => (prevCount + deltaTime * 0.01) % 100);
+    }
+    previousTimeRef.current = time;
+  });
 
-    let video = videoRef.current;
-    let photo = photoRef.current;
+  return <div>{Math.round(count)}</div>;
 
-    photo.width = width;
-    photo.height = height;
+  // const videoRef = useRef(null);
+  // const photoRef = useRef(null);
 
-    let ctx = photo.getContext("2d");
-    ctx.drawImage(video, 0, 0, width, height);
+  // const [hasPhoto, setHasPhoto] = useState(false);
 
-    setHasPhoto(true);
-  };
+  // const getVideo = () => {
+  //   navigator.mediaDevices
+  //     .getUserMedia({ video: { width: 1920, height: 1080 } })
+  //     .then((stream) => {
+  //       let video = videoRef.current;
+  //       video.srcObject = stream;
+  //       video.play();
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // };
 
-  const closePhoto = () => {
-    let photo = photoRef.current;
-    let ctx = photo.getContext("2d");
+  // const takePhoto = () => {
+  //   const width = 414;
+  //   const height = width / (16 / 9);
 
-    ctx.clearRect(0, 0, photo.width, photo.height);
+  //   // let video = videoRef.current;
+  //   let photo = photoRef.current;
 
-    setHasPhoto(false);
-  };
+  //   photo.width = width;
+  //   photo.height = height;
 
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
+  //   setHasPhoto(true);
 
-  const videoStyle = {
-    width: "100%",
-    maxWidth: "100%",
-    height: "auto",
-  };
+  //   // let ctx = photo.getContext("2d");
+  //   // ctx.drawImage(video, 0, 0, width, height);
 
-  return (
-    <>
-      <div className="camera">
-        <video ref={videoRef} style={videoStyle} />
-        <button onClick={takePhoto}>SNAP!</button>
-      </div>
-      <div className="result">
-        <canvas ref={photoRef} />
-        <button onClick={closePhoto}>CLOSE!</button>
-      </div>
-    </>
-  );
+  //   streaming();
+  // };
+
+  // const streaming = () => {
+  //   if (!hasPhoto) {
+  //     return;
+  //   }
+  //   let photo = photoRef.current;
+  //   photo
+  //     .getContext("2d")
+  //     .drawImage(videoRef.current, 0, 0, photo.width, photo.height);
+  //   requestAnimationFrame(streaming);
+  // };
+
+  // const closePhoto = () => {
+  //   let photo = photoRef.current;
+  //   let ctx = photo.getContext("2d");
+
+  //   ctx.clearRect(0, 0, photo.width, photo.height);
+
+  //   setHasPhoto(false);
+  // };
+
+  // useEffect(() => {
+  //   getVideo();
+  // }, [videoRef]);
+
+  // const videoStyle = {
+  //   width: "100%",
+  //   maxWidth: "100%",
+  //   height: "auto",
+  // };
+
+  // return (
+  //   <>
+  //     <div className="camera">
+  //       <video ref={videoRef} style={videoStyle} />
+  //       <button onClick={takePhoto}>SNAP!</button>
+  //     </div>
+  //     <div className="result">
+  //       <canvas ref={photoRef} />
+  //       <button onClick={closePhoto}>CLOSE!</button>
+  //     </div>
+  //   </>
+  // );
 };
 
 export default QrcodeReader;
